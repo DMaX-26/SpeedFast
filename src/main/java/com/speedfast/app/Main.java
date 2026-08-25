43491d762220c19341f5759ba97ddac0bc45d9d6
Max <ma.alveare@duocuc.cl>
2026-08-24 23:01:12 -0400

    Actualizacion del Main con llamado de nuevos metodos

--- src/main/java/com/speedfast/app/Main.java
+++ src/main/java/com/speedfast/app/Main.java
@@ -9,42 +9,48 @@ public class Main {
 
         System.out.println("---Pedido de comida---");
         //Se crea una instancia de PedidoComida y se ingresan los datos
-        PedidoComida pedidoComida1 = new PedidoComida(1, "Los álamos 123, Viña del Mar", "comida");
-        //Se llama al metodo (versión sobreescrita)
-        pedidoComida1.asignarRepartidor();
-        //Se llama al metodo (versión sobrecargada)
+        PedidoComida pedidoComida1 = new PedidoComida(1, "Los álamos 123, Viña del Mar", 2.4);
+        //Se llama al metodo asignarRepartidor (version sobreescrita)
         pedidoComida1.asignarRepartidor("Pablo Gonzalez");
+        //Se llama al metodo mostrarResumen
+        pedidoComida1.mostrarResumen();
+        //Se llama al metodo calcularTiempoEntrega
+        pedidoComida1.calcularTiempoEntrega();
         //Salto de línea
         System.out.println();
 
         System.out.println("---Pedido de comida---");
-        PedidoComida pedidoComida2 = new PedidoComida(2, "Calle Santiago 789, Con con", "comida");
-        pedidoComida2.asignarRepartidor();
+        PedidoComida pedidoComida2 = new PedidoComida(2, "Calle Santiago 789, Con-con", 1.5);
         pedidoComida2.asignarRepartidor("Hector Rodriguez");
+        pedidoComida2.mostrarResumen();
+        pedidoComida2.calcularTiempoEntrega();
         System.out.println();
 
         System.out.println(":::Pedido de encomienda:::");
-        PedidoEncomienda pedidoEncomienda1 = new PedidoEncomienda(21, "Pasaje el roble 321, Quilpué", "encomienda");
-        pedidoEncomienda1.asignarRepartidor();
+        PedidoEncomienda pedidoEncomienda1 = new PedidoEncomienda(21, "Pasaje el roble 321, Quilpué", 4.5);
         pedidoEncomienda1.asignarRepartidor("Martina Fernandez");
+        pedidoEncomienda1.mostrarResumen();
+        pedidoEncomienda1.calcularTiempoEntrega();
         System.out.println();
 
         System.out.println(":::Pedido de encomienda:::");
-        PedidoEncomienda pedidoEncomienda2 = new PedidoEncomienda(22, "La Retuca 456, Peñablanca", "encomienda");
-        pedidoEncomienda2.asignarRepartidor();
+        PedidoEncomienda pedidoEncomienda2 = new PedidoEncomienda(22, "La Retuca 456, Peñablanca", 4);
         pedidoEncomienda2.asignarRepartidor("Francisco Romero");
+        pedidoEncomienda2.mostrarResumen();
+        pedidoEncomienda2.calcularTiempoEntrega();
         System.out.println();
 
         System.out.println("***Pedido express***");
-        PedidoExpress pedidoExpress1 = new PedidoExpress(12, "Calle Venecia 012, Valparaiso", "express");
-        pedidoExpress1.asignarRepartidor();
+        PedidoExpress pedidoExpress1 = new PedidoExpress(12, "Calle Venecia 012, Valparaiso", 6);
         pedidoExpress1.asignarRepartidor("Pedro Suarez");
+        pedidoExpress1.mostrarResumen();
+        pedidoExpress1.calcularTiempoEntrega();
         System.out.println();
 
         System.out.println("***Pedido express***");
-        PedidoExpress pedidoExpress2 = new PedidoExpress(13, "Calle Santa Maria 034, Valparaiso", "express");
-        pedidoExpress2.asignarRepartidor();
+        PedidoExpress pedidoExpress2 = new PedidoExpress(13, "Calle Santa Maria 034, Valparaiso", 2);
         pedidoExpress2.asignarRepartidor("Mario Gonzalez");
-
+        pedidoExpress2.mostrarResumen();
+        pedidoExpress2.calcularTiempoEntrega();
     }
 }
