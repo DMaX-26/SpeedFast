3520dfc0e8e48e13cb24771a19a3a79770b08f5f
Max <ma.alveare@duocuc.cl>
2026-08-24 20:24:47 -0400

    Actualizacion a clase abstracta e incorporacion de metodos implementados y abstractos

--- src/main/java/com/speedfast/model/Pedido.java
+++ src/main/java/com/speedfast/model/Pedido.java
@@ -1,26 +1,43 @@
 package com.speedfast.model;
 
 /**
- * Clase que representa un Pedido
+ * Clase padre abstracta que representa un Pedido
  */
-public class Pedido {
-    private int idPedido;
-    private String direccionEntrega;
-    private String tipoPedido;
+public abstract class Pedido {
+    /**
+     * Atributos de tipo protected para acceder a ellos desde cada subclase
+     */
+    protected int idPedido;
+    protected String direccionEntrega;
+    protected double distanciaKm;
 
     /**
      * Constructor
      * @param idPedido
      * @param direccionEntrega
-     * @param tipoPedido
+     * @param distanciaKm
      */
-    public Pedido(int idPedido, String direccionEntrega, String tipoPedido) {
+    public Pedido(int idPedido, String direccionEntrega, double distanciaKm) {
         this.idPedido = idPedido;
         this.direccionEntrega = direccionEntrega;
-        this.tipoPedido = tipoPedido;
+        this.distanciaKm = distanciaKm;
     }
 
     public void asignarRepartidor(){
         System.out.println("Asignando repartidor...");
     }
+
+    /**
+     * Metodo implementado
+     */
+    public void mostrarResumen(){
+        System.out.println("Código Pedido: "+idPedido);
+        System.out.println("Dirección de entrega: "+direccionEntrega);
+        System.out.println("Distancia en Km: "+distanciaKm);
+    }
+
+    /**
+     * Metodo abstracto que debe ser implementado en cada subclase
+     */
+    public abstract void calcularTiempoEntrega();
 }
