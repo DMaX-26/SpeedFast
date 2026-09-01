126b69220d7956713275c473b0033afd9d9d8d75
Max <ma.alveare@duocuc.cl>
2026-08-31 23:29:55 -0400

    Se actualiza la clase Pedido

--- src/main/java/com/speedfast/model/Pedido.java
+++ src/main/java/com/speedfast/model/Pedido.java
@@ -1,13 +1,20 @@
 package com.speedfast.model;
 
+import com.speedfast.interfaces.Rastreable;
+import com.speedfast.interfaces.Cancelable;
+import com.speedfast.interfaces.Despachable;
+
 /**
  * Clase padre abstracta que representa un Pedido
+ * Implementa las interfaces Despachable, Cancelable, Rastreable
+ * Cada subclase debe implementar los métodos correspondientes a cada interfaz
  */
-public abstract class Pedido {
+public abstract class Pedido implements Despachable, Cancelable, Rastreable {
     /**
      * Atributos de tipo protected para acceder a ellos desde cada subclase
      */
     protected int idPedido;
+    protected String tipoPedido;
     protected String direccionEntrega;
     protected double distanciaKm;
 
@@ -17,8 +24,9 @@ public abstract class Pedido {
      * @param direccionEntrega
      * @param distanciaKm
      */
-    public Pedido(int idPedido, String direccionEntrega, double distanciaKm) {
+    public Pedido(int idPedido, String tipoPedido, String direccionEntrega, double distanciaKm) {
         this.idPedido = idPedido;
+        this.tipoPedido = tipoPedido;
         this.direccionEntrega = direccionEntrega;
         this.distanciaKm = distanciaKm;
     }
@@ -27,17 +35,22 @@ public abstract class Pedido {
         System.out.println("Asignando repartidor...");
     }
 
+    public abstract void asignarRepartidor(String nombreRepartidor);
+
     /**
      * Metodo implementado
      */
     public void mostrarResumen(){
+        System.out.println("---"+tipoPedido+"---");
         System.out.println("Código Pedido: "+idPedido);
         System.out.println("Dirección de entrega: "+direccionEntrega);
         System.out.println("Distancia en Km: "+distanciaKm);
     }
 
     /**
-     * Metodo abstracto que debe ser implementado en cada subclase
+     * Metodos abstractos que deben ser implementados en cada subclase
      */
     public abstract void calcularTiempoEntrega();
+
+    public abstract void mostrarHistorial();
 }
