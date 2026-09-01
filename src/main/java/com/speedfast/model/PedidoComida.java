2e2474299532c28ea925bc225ed50e2dcc82a2bf
Max <ma.alveare@duocuc.cl>
2026-08-31 23:30:59 -0400

    Se actualiza la clase PedidoComida

--- src/main/java/com/speedfast/model/PedidoComida.java
+++ src/main/java/com/speedfast/model/PedidoComida.java
@@ -1,10 +1,18 @@
 package com.speedfast.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Subclase que representa un Pedido de comida
  * Hereda atributos y métodos de la clase padre Pedido
+ * Implementa los métodos correspondientes a las interfaces Despachable, Cancelable y Rastreable
  */
-public class PedidoComida extends Pedido{
+public class PedidoComida extends Pedido {
+    /**
+     * Se crea un atributo de tipo List<String> correspondiente a la interfaz Rastreable
+     */
+    private List<String> historial;
 
     /**
      * Constructor para crear los objetos e inicializar atributos heredados
@@ -12,11 +20,15 @@ public class PedidoComida extends Pedido{
      * @param direccionEntrega
      * @param distanciaKm
      */
-    public PedidoComida(int idPedido, String direccionEntrega, double distanciaKm) {
+    public PedidoComida(int idPedido, String tipoPedido, String direccionEntrega, double distanciaKm) {
         /**
          * Llamado al constructor de la clase padre para inicializar sus atributos en el objeto actual
          */
-        super(idPedido, direccionEntrega, distanciaKm);
+        super(idPedido, tipoPedido, direccionEntrega, distanciaKm);
+        this.historial = new ArrayList<>();
+        //Se agrega el texto "Pedido + id" a la lista historial
+        this.historial.add("Número de pedido: "+idPedido);
+        this.historial.add("Distancia: "+distanciaKm+" km");
     }
 
     /**
@@ -42,17 +54,62 @@ public class PedidoComida extends Pedido{
          */
         int tiempoEstimado = (int) tiempo;
 
+        System.out.println("---------------------------------------");
         System.out.println("Tiempo estimado de entrega: "+tiempoEstimado+" minutos");
+        System.out.println("---------------------------------------");
+        System.out.println();
     }
 
     /**
      * Sobrecarga del metodo asignarRepartidor
      * @param nombreRepartidor
      */
+    @Override
     public void asignarRepartidor(String nombreRepartidor){
         System.out.println("Repartidor disponible: "+nombreRepartidor);
         System.out.println("Verificando si el repartidor tiene mochila térmica...");
         System.out.println("El repartidor sí tiene mochila térmica!");
         System.out.println("El pedido ha sido asignado a: "+nombreRepartidor);
+        System.out.println();
+        historial.add("Repartidor asignado: "+nombreRepartidor);
+    }
+
+    @Override
+    public void despachar() {
+        System.out.println("--------------------------------------------");
+        System.out.println("Despachando pedido de comida "+"'"+idPedido+"'"+"...");
+        System.out.println("... ... ...");
+        System.out.println("El pedido de comida "+"'"+idPedido+"'"+" ha sido despachado!");
+        System.out.println("--------------------------------------------");
+        System.out.println();
+        //Se agrega el texto "Pedido despachado + direccionEntrega" a la lista historial
+        historial.add("Pedido despachado a: "+direccionEntrega);
+    }
+
+    @Override
+    public void cancelar() {
+        System.out.println("----------------------------------------------------------");
+        System.out.println("Cancelando pedido "+"'"+idPedido+"'");
+        System.out.println("... ... ...");
+        System.out.println("El pedido de comida "+"'"+idPedido+"'"+" ha sido cancelado correctamente!");
+        System.out.println("----------------------------------------------------------");
+        System.out.println();
+        //Se agrega el texto "Pedido cancelado" a la lista historial
+        historial.add("Pedido cancelado");
+    }
+
+    @Override
+    public List<String> verHistorial() {
+        return historial;
+    }
+
+    public void mostrarHistorial(){
+        /**
+         * Se recorre la lista y se imprime
+         */
+        for (String elemento : verHistorial()) {
+            System.out.println(elemento);
+        }
+        System.out.println();
     }
 }
